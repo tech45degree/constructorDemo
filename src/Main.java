332224a69cd6cfd9a548b3aac68f83b990e86c2e
Muhammad Faisal <faisal.khan16@hotmail.com>
2023-05-31 23:34:18 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Person obj = new Person("Micky",31);
+
+        System.out.println(obj.toString());
+
+    }
+
+}
---
+++ src/Person.java
@@ -0,0 +1,54 @@
+public class Person {
+
+    private String name;
+    private  int age;
+    private int weight;
+
+    public Person(){
+
+    }
+
+    public Person(String name,int age,int weight){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                '}';
+    }
+}
